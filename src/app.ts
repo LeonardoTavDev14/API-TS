e6ef87e1610a85cad7133861815c726064155468
LeonardoTavDev14 <contatoleonardotavdev@gmail.com>
2025-07-08 00:36:15 -0300

    feat:add-rate-limit

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { userRoutes } from "./interface/routes/user/routes.user";
+import { limiter } from "./shared/config/rateLimiter";
 
 const app = express();
 
 app.use(express.json());
 app.use("/auth/user", userRoutes);
+app.use(limiter);
 
 export { app };
